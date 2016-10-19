72b6daa943b09c521721a58c8c5d0fb7042d66d0
joce <jocelynlegault@gmail.com>
2016-10-19 12:50:24 -0400

    Add build.bat file that builds project using MSBuild
    Signed-off-by: joce <joce@unity3d.com>

---
+++ build.bat
@@ -0,0 +1,3 @@
+"set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319" 
+call %msBuildDir%\msbuild.exe ExCSS.Unity.sln  /p:Configuration=Release /l:FileLogger,Microsoft.Build.Engine;logfile=Manual_MSBuild_ReleaseVersion_LOG.log
+
