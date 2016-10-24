1eea297d27696e54f9377e5fef422d2a849acfde
joce <jocelynlegault@gmail.com>
2016-10-24 10:53:56 -0400

    Unit test runner enabled.
    - Add NUnit runner v3.5 to run unit tests.
    - Modify build.bat to run the said unit tests.
    
    Signed-off-by: joce <joce@unity3d.com>

--- build.bat
+++ build.bat
@@ -1,3 +1,11 @@
+REM Build library
 set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 call %msBuildDir%\msbuild.exe ExCSS.Unity.sln  /p:Configuration=Release /l:FileLogger,Microsoft.Build.Engine;logfile=Manual_MSBuild_ReleaseVersion_LOG.log
+if ERRORLEVEL 1 exit 1
 
+REM Run unit tests
+call .\nunit-console\nunit3-console.exe .\ExCSS.Tests\bin\DebugUnity35\ExCSS.Tests.dll
+if ERRORLEVEL 1 exit 1
+
+call .\nunit-console\nunit3-console.exe .\ExCSS.Tests\bin\ReleaseUnity35\ExCSS.Tests.dll
+if ERRORLEVEL 1 exit 1
