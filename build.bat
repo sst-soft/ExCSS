3bb3ce0027d2f7542530e29e4d47c6b203ac09e3
joce <jocelynlegault@gmail.com>
2017-01-09 14:03:03 -0500

    Update build.bat to generate builds.zip file
    Signed-off-by: joce <joce@unity3d.com>

--- build.bat
+++ build.bat
@@ -1,11 +1,44 @@
-REM Build library
+@ECHO OFF
+
+ECHO.
+ECHO ==[Building library]==
+ECHO.
 set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 call %msBuildDir%\msbuild.exe ExCSS.Unity.sln  /p:Configuration=Release /l:FileLogger,Microsoft.Build.Engine;logfile=Manual_MSBuild_ReleaseVersion_LOG.log
 if ERRORLEVEL 1 exit 1
 
-REM Run unit tests
+ECHO.
+ECHO ==[Run Debug unit tests]==
+ECHO.
 call .\nunit-console\nunit3-console.exe .\ExCSS.Tests\bin\DebugUnity35\ExCSS.Tests.dll
 if ERRORLEVEL 1 exit 1
 
+ECHO.
+ECHO ==[Run Release unit tests]==
+ECHO.
 call .\nunit-console\nunit3-console.exe .\ExCSS.Tests\bin\ReleaseUnity35\ExCSS.Tests.dll
+if ERRORLEVEL 1 exit 1
+
+ECHO.
+ECHO ==[Prepare DLLs for zipping]==
+ECHO.
+ECHO [Clear .\builds dir]
+rmdir /S /Q .\builds
+if ERRORLEVEL 1 exit 1
+
+ECHO [Remove old builds.zip]
+del /Q .\builds.zip
+
+ECHO [Make .\builds\lib\net35 dir]
+mkdir .\builds\lib\net35
+if ERRORLEVEL 1 exit 1
+
+ECHO [Copy files to .\builds\lib\net35 dir]
+copy .\ExCSS\bin\ReleaseUnity35\*.* .\builds\lib\net35
+if ERRORLEVEL 1 exit 1
+
+ECHO.
+ECHO ==[Zip files]==
+ECHO.
+call 7z.exe a .\builds.zip .\builds
 if ERRORLEVEL 1 exit 1
